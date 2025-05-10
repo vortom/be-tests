852906d466097298092887b7e09261bc8a03f5c7
Tomas Vorel <fefeczech@gmail.com>
2025-05-10 11:58:02 +0200

    Copilot: add Store endpoints

--- api/store.py
+++ api/store.py
@@ -1,2 +1,65 @@
-class StoreApi():
-    pass
+from common.http_client import HttpClient
+from urllib.parse import urljoin
+
+
+class StoreApi(HttpClient):
+    # https://petstore.swagger.io/#/store/
+
+    def __init__(self, host: str | None, headers: dict | None = None):
+        super().__init__()
+        self.host = host
+        self.headers = headers or {}
+
+    def get_inventory(self, expected_status_code: int = 200, **kwargs):
+        # https://petstore.swagger.io/#/store/getInventory
+        endpoint = "/v2/store/inventory"
+
+        return self.rest(
+            method="GET",
+            url=urljoin(self.host, endpoint),
+            headers={**kwargs.get("headers", {}), **self.headers},
+            params=kwargs.get("params", []),
+            name=endpoint,
+            expected_status_code=expected_status_code
+        )
+
+    def place_order(self, expected_status_code: int = 200, **kwargs):
+        # https://petstore.swagger.io/#/store/placeOrder
+        endpoint = "/v2/store/order"
+
+        return self.rest(
+            method="POST",
+            url=urljoin(self.host, endpoint),
+            headers={**kwargs.get("headers", {}), **self.headers},
+            params=kwargs.get("params", []),
+            data=kwargs.get("data", None),
+            json=kwargs.get("json", {}),
+            name=endpoint,
+            expected_status_code=expected_status_code
+        )
+
+    def get_order_by_id(self, order_id: int, expected_status_code: int = 200, **kwargs):
+        # https://petstore.swagger.io/#/store/getOrderById
+        endpoint = f"/v2/store/order/{order_id}"
+
+        return self.rest(
+            method="GET",
+            url=urljoin(self.host, endpoint),
+            headers={**kwargs.get("headers", {}), **self.headers},
+            params=kwargs.get("params", []),
+            name=endpoint,
+            expected_status_code=expected_status_code
+        )
+
+    def delete_order(self, order_id: int, expected_status_code: int = 200, **kwargs):
+        # https://petstore.swagger.io/#/store/deleteOrder
+        endpoint = f"/v2/store/order/{order_id}"
+
+        return self.rest(
+            method="DELETE",
+            url=urljoin(self.host, endpoint),
+            headers={**kwargs.get("headers", {}), **self.headers},
+            params=kwargs.get("params", []),
+            name=endpoint,
+            expected_status_code=expected_status_code
+        )
--- docs/prompts.md
+++ docs/prompts.md
@@ -5,3 +5,9 @@
 ```
 The 'petstore_swagger.json' file contains the OpenAPI specification, and 'pet.py' file includes a wrapper to call this API from Python tests. Please review the OpenAPI specification and implement Python methods for any endpoints that have not been implemented yet. Thank you.
 ```
+
+## Implement New API Methods Based on OpenAPI Specification 
+
+```
+The 'petstore_swagger.json' file contains the OpenAPI specification. The 'pet.py' file contains an example implementation of methods used to call endpoints from tests. Please review both files and implement methods for the 'Store' endpoints according to the OpenAPI specification, following the same implementation style. Save the new implementation in the 'store.py' file. Thank you!
+```
